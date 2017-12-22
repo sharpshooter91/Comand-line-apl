ac0d4f5c03e0fa36ca73543e846500c04d1bea9c
sharpshooter91 <janko307@gmail.com>
2017-12-22 22:06:34 +0100

    Making apl flexible to multiple usernames
    Correcting code and adding function getProfile to make apl accept multiple usernames as argument to function getProfile

--- Stage 1/Comand line aplication/app.js
+++ Stage 1/Comand line aplication/app.js
@@ -2,30 +2,32 @@
 // Solution: Use Node.js to connect to Treehouse's API to get profile information to print out
 //Require https
 const https= require("https");
-const username="chalkers";
 
 function printMessage(username, badgecount, points) {
   const message= `${username} has ${badgecount} total badge(s) and ${points} points in Javascript`;
   console.log(message);
 }
 
+function getProfile(username){
 //connect to the api url(https://teamtreehouse.com/username.json)
 const request= https.get(`https://teamtreehouse.com/${username}.json`, response=> {
-  let body= "";
-  //read the data
-  response.on('data', data=> {
-    body+= data.toString();
-  });
+            let body= "";
+            //read the data
+            response.on('data', data=> {
+              body+= data.toString();
+            });
 
-  response.on("end", ()=> {
-    //Parse the data
-    const profile= JSON.parse(body);
-    //console.dir(profile);
-    //Print out the data
-    printMessage(username, profile.badges.length, profile.points.JavaScript);
+            response.on("end", ()=> {
+              //Parse the data
+              const profile= JSON.parse(body);
+              //console.dir(profile);
+              //Print out the data
+              printMessage(username, profile.badges.length, profile.points.JavaScript);
 
-  });
-  //Parse the data
-  //Print out the data
+            });
 
-});
+          });
+};
+
+getProfile("chalkers");
+getProfile("alenaholligan");
